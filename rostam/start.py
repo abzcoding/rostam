a6200867ce370c542ac277a6fb6fd9c44d989698
Abouzar Parvan <abzcoding@gmail.com>
2016-07-08 23:30:15 +0430

    trying to use dynamic loading

---
+++ rostam/start.py
@@ -0,0 +1,83 @@
+# Import Python libs
+import argparse
+import logging
+from time import sleep
+
+from rostam.utils.constants import Settings
+
+
+def auto_build(base_dir="/opt/rostam/", db_type="sqlite", db_name="rostam", db_username=None, db_password=None,
+               job_runner="local", log_file="rostam.log"):
+    # set the default db
+    if str(db_type).lower() == "mysql":
+        db_choice = "rostam.db.mysql.Database"
+    elif str(db_type).lower() == "postgresql":
+        db_choice = "rostam.db.postgresql.Database"
+    else:
+        db_choice = "rostam.db.sqlite.Database"
+    # set the default runner module
+    if str(job_runner).lower() == "saltstack":
+        runner_choice = "rostam.schedule.runners.saltstack.Runner"
+    elif str(job_runner).lower() == "ansible":
+        runner_choice = "rostam.schedule.runners.ansible.Runner"
+    else:
+        runner_choice = "rostam.schedule.runners.local.Runner"
+    Settings(db_connector=db_choice, base_folder=base_dir, runner=runner_choice, log_file=log_file)
+
+
+def create_jobs(db, sched, runner):
+    rows = db.db.query('SELECT * FROM containers')
+    for r in rows:
+        container_id = db.get_container_id(container_name=r['name'], container_tag=r['tag'])
+        repo_id, repo = db.get_container_repo_id(container_name=r['name'], container_tag=r['tag'])[0]
+        repo_path = BASE_FOLDER + "repos/" + str(r['name']) + '-' + str(r['tag'])
+        if repo_id > 0:
+            sched.add_job(job_type=runner.pull, minutes=(int(r['interval']) * 2) / 3,
+                          job_id="pull-" + str(r['name']) + '-' + str(r['tag']),
+                          args=[container_id, repo, repo_path])
+            sched.add_job(job_type=runner.build, minutes=r['interval'],
+                          job_id="build-" + str(r['name']) + '-' + str(r['tag']),
+                          args=[container_id, repo, repo_path, r['timeout'], r['tag']])
+            set_container_status(container_id=container_id, enabled=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='automatic docker pull and build system aka "rostam".')
+    parser.add_argument('-l', '--location', help='default directory for this application')
+    parser.add_argument('-db', '--database', choices=['sqlite', 'postgresql', 'mysql'], help='database type')
+    parser.add_argument('-r', '--runner', choices=['local', 'ansible', 'saltstack'], help='runner type')
+    parser.add_argument('--dbname', help="name of the default database")
+    parser.add_argument('--dbuser', help="username of the database")
+    parser.add_argument('--dbpass', help="password of the database")
+    args = parser.parse_args()
+
+    auto_build(base_dir=args.location, db_type=args.database, job_runner=args.runner,
+               db_name=args.dbname, db_username=args.dbuser, db_password=args.dbpass)
+    log = logging.getLogger(__name__)
+    '''
+    from rostam.db.sqlite import Database
+    from rostam.schedule.runners.local import Runner
+    DB_CONNECTOR = Database
+    BASE_FOLDER = "/opt/rostam/"
+    '''
+    # sync repos from properties file
+    runner = RUNNER()
+    from rostam.utils.file_sync import sync_properties
+    sync_properties(properties_file=BASE_FOLDER + "containers.properties")
+    db = Database(location=BASE_FOLDER + "rostam.db")
+    from rostam.schedule.async import Scheduler
+    sched = Scheduler()
+    try:
+        while True:
+            create_jobs(db, sched, runner)
+            sleep(60)
+    except (KeyboardInterrupt, SystemExit):
+        try:
+            sched.shutdown()
+            db.db.close()
+        except Exception:
+            pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ rostam/utils/constants.py
@@ -0,0 +1,41 @@
+from importlib import import_module
+
+
+class Settings(object):
+
+    def __init__(self, db_connector=None, base_folder="", runner=None, log_file="rostam.log"):
+        Settings.db_connector = db_connector
+        Settings.base_folder = base_folder
+        Settings.runner = runner
+        Settings.log_file = log_file
+
+    @staticmethod
+    def DB_CONNECTOR():
+        return Settings.db_connector
+
+    @staticmethod
+    def BASE_FOLDER():
+        return Settings.base_folder
+
+    @staticmethod
+    def LOG_FILENAME():
+        try:
+            return Settings.log_file
+        except AttributeError:
+            return "rostam.log"
+
+    @staticmethod
+    def RUNNER():
+        return Settings.runner
+
+
+class Loader(object):
+
+    @staticmethod
+    def class_loader(class_string):
+        full_class_string = class_string
+        class_data = full_class_string.split(".")
+        module_path = ".".join(class_data[:-1])
+        class_str = class_data[-1]
+        module = import_module(module_path)
+        return getattr(module, class_str)
