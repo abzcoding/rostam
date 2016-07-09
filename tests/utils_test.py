2c5ea4893586c188f6b4bee7d61609718ff0a726
Abouzar Parvan <abzcoding@gmail.com>
2016-07-09 14:19:28 +0430

    fixed some bugs in test

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,4 +1,5 @@
 import unittest
+from os import path
 
 from rostam.db.sqlite import Database
 from rostam.utils.constants import Settings
@@ -10,18 +11,17 @@ class FileSyncTest(unittest.TestCase):
     def test_read_properties_file(self):
         res = read_properties_file()
         self.assertTrue(len(res['hello world:latest']) == 4)
-        self.assertEqual(res['hello world:latest']['interval'], 60)
+        self.assertEqual(res['hello world:latest']['interval'], 666)
         self.assertEqual(res['hello world:latest']['tag'], 'latest')
-        self.assertEqual(res['hello world:latest']['repo'], 'https://github.com/docker-library/hello-world.git')
+        self.assertEqual(res['hello world:latest']['repo'], 'https://github.com/abzcoding/test.git')
 
     def test_sync_properties(self):
         sync_properties()
-        db = Database(location=Settings.BASE_FOLDER() + "rostam.db")
+        db = Database(location=path.join(Settings.BASE_FOLDER(), "rostam.db"))
         res = db.db.query('select * from vcs')
         self.assertEqual(res[0]['id'], 1)
-        self.assertEqual(res[0]['repo'], 'https://github.com/docker-library/hello-world.git')
+        self.assertEqual(res[0]['repo'], 'https://github.com/abzcoding/test.git')
         self.assertIsNone(res[0]['built_revision'])
-        self.assertIsNone(res[0]['latest_revision'])
         self.assertEqual(res[0]['container_id'], 1)
         db.delete_db()
 
