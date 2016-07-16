1af6a31dafc40a9b9d4a4f6ffb6fcf5d63b8ca3c
Abouzar Parvan <abzcoding@users.noreply.github.com>
2016-07-16 09:43:57 +0430

    log to console as well as file

--- rostam/utils/log_setup.py
+++ rostam/utils/log_setup.py
@@ -10,7 +10,10 @@ handler = logging.handlers.RotatingFileHandler(
 format = "%(asctime)s %(name)-20s %(levelname)-8s %(message)s"
 handler.setFormatter(logging.Formatter(format))
 logging.getLogger('').addHandler(handler)
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logging.Formatter(format))
 
 # add handler to python logger as soon as this module is imported
 if len(logging.root.handlers) == 0:
     logging.root.addHandler(handler)
+    logging.root.addHandler(consoleHandler)
